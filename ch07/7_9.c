940fa966102bb5b0280823d9faa99d34e04b108e
pytlab <shaozhengjiang@gmail.com>
2015-05-19 09:56:45 +0800

    Initial commit

---
+++ ch07/7_9.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int count, i;
+    int isprime(int);
+
+    puts("Enter a integer: ");
+    scanf("%d", &i);
+
+    if(i == 2)
+    {
+        printf("2\n");
+        return 0;
+    }
+    else
+    {
+        printf("2 ");
+        for(count = 3; count <= i; count += 2)
+        {
+            if(isprime(count))
+                printf("%d ", count);
+            else
+                continue;
+        }
+        putchar('\n');
+    }
+
+    return 0;
+}
+
+int isprime(int number)
+{
+    int i;
+
+    for(i = 2; i <= sqrt(number); i++)
+    {
+        if(number % i == 0)
+            return 0;
+        else
+            continue;
+    }
+
+    return 1;
+}
