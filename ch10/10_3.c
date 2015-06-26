3800f613bd7a8f7532d43478d393eb51a0b699f3
pytlab <shaozhengjiang@gmail.com>
2015-06-26 22:22:15 +0800

    Add const to protect array

--- ch10/10_3.c
+++ ch10/10_3.c
@@ -4,7 +4,7 @@ int main(void)
 {
     int arr[10] = {1, 2, 3, 4, 20, 5, 6, 7, 8, 9};
     int res;
-    int get_max(int *, int n);
+    int get_max(const int *, int n);
 
     res = get_max(arr, 10);
     printf("The max number is: %d.\n", res);
@@ -12,7 +12,7 @@ int main(void)
     return 0;
 }
 
-int get_max(int * arr, int n)
+int get_max(const int * arr, int n)
 {
     int i, max = arr[0];
 
